aa1604e6caf697e09ea4daed85d35b57f4bdf26a
Nimrod Dolev <nimrodavid@gmail.com>
2018-09-12 17:08:08 +0200

    fixed namespace

--- src/SimplyTyped/Serialization/PrimitiveAttributeSerializer.cs
+++ src/SimplyTyped/Serialization/PrimitiveAttributeSerializer.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SimplyTyped
+namespace SimplyTyped.Serialization
 {
     internal class PrimitiveAttributeSerializer : ISerializer
     {

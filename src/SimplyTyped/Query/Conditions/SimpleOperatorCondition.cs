d987bf5ccbd873c347738635f918be5bfd7d4ae0
Nimrod Dolev <nimrodavid@gmail.com>
2018-09-15 00:26:12 +0200

    More simple conversion support (left out of commit by mistake)

---
+++ src/SimplyTyped/Query/Conditions/SimpleOperatorCondition.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using SimplyTyped.Core.Query;
+using SimplyTyped.Utils;
+
+namespace SimplyTyped.Query
+{
+    internal class SimpleOperatorCondition : ICondition
+    {
+        private static readonly Dictionary<SimpleOperator, string> OPERATORS = new Dictionary<SimpleOperator, string>
+        {
+            [SimpleOperator.IsEqualTo] = "=",
+            [SimpleOperator.GreaterThan] = ">",
+            [SimpleOperator.GreaterThanOrEqualTo] = ">=",
+            [SimpleOperator.LessThan] = "<",
+            [SimpleOperator.LessThanOrEqualTo] = "<="            
+        };
+
+        private const string TEMPLATE = "`{0}` {1} '{2}'";
+
+        private string _operator;
+        private string _member;
+        private string _value;
+        public SimpleOperatorCondition(SimpleOperator @operator, string member, string value)
+        {
+            if (!OPERATORS.TryGetValue(@operator, out _operator))
+                throw new Exception($"Unknown operator: {@operator.ToString()}");
+            _member = member;
+            _value = value;
+        }
+
+        public string Condition => string.Format(TEMPLATE, _member, _operator, QueryEncodingUtility.EncodeValue(_value));
+    }
+}
